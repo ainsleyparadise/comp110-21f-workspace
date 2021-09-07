f01bc3487222cc65b18905feba1fb7a127aa68a8
Ainsley Paradise <ainsleyp@email.unc.edu>
2021-09-07 19:07:42 -0400

    progress on exercise 2

--- exercises/ex02/count_letters.py
+++ exercises/ex02/count_letters.py
@@ -4,13 +4,14 @@ __author__ = "730402784"
 
 
 # Begin your solution here...
+from typing import Counter
 
 question: str = input("What letter do you want to search for?: ")
 word: str = input("Enter a word: ")
 i: int = 0
 length: int = len(word)
 
-
-while i < length:
-    print("Count: " + str(i))
-    i = i + 1
+c = Counter(word)
+count: int = (c[question])
+print("Count: " + str(count))
+ 
