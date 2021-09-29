df3d854c063f6aa0df7c1b0bcdcbfee0bdeccb69
Ainsley Paradise <ainsleyp@email.unc.edu>
2021-09-29 13:07:03 -0400

    progress

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -4,3 +4,12 @@ __author__ = "123456789"
 
 
 # TODO: Implement your functions here.
+
+number: int = int(input("What number do you want to match?"))
+
+
+def all(xs: list[int]) -> None:
+    i: int = 0
+    while i < len[xs]:
+        if xs[i] == number is True:
+            print(True)
