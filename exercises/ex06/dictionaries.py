369b67f654ab4febd38e9ac55a09b04da80c8932
Ainsley Paradise <ainsleyp@email.unc.edu>
2021-10-31 16:38:09 -0400

    progress

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -1,5 +1,49 @@
 """Practice with dictionaries."""
 
-__author__ = "123456789"
+__author__ = "730402784"
 
 # Define your functions below
+
+
+def invert(dict1: dict[str, str]) -> dict[str, str]:
+    """Function that inverts the keys and values of a dictionary."""
+    dict2: dict[str, str] = {}
+    for key in dict1:
+        new_key: str = dict1[key]
+        if new_key in dict2:
+            raise KeyError("Each key must be unique!")
+        dict2[new_key] = key
+
+    return dict2
+
+
+def favorite_color(dict3: dict[str, str]) -> str:
+    """Function that returns the common favorite color."""
+    dict4: dict[str, str] = {}
+    color = str(dict4)
+    i = 0
+    j = 1
+    for key in dict3:
+        if dict3[key] == dict3[key]:
+            color = dict3[key]
+            i = i + 1
+            j = j + 1
+        else:
+            i = i + 1
+            j = j + 1
+    
+    return color
+
+
+def count(list1: list[str]) -> dict[str, int]:
+    """Function that counts the number of times a value is present."""
+    dict5: dict[str, int] = {}
+    i = 0
+    while i < len(list1):
+        if list1[i] in dict5:
+            dict5[list1[i]] += 1
+        else:
+            dict5[list1[i]] = 1
+        i += 1
+
+    return dict5
--- exercises/ex06/dictionaries_test.py
+++ exercises/ex06/dictionaries_test.py
@@ -1,6 +1,60 @@
 """Unit tests for dictionary functions."""
 
 # TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex06.dictionaries import invert, favorite_color, count
+from exercises.ex06.dictionaries import invert, favorite_color, count
 
-__author__ = "123456789"
+__author__ = "730402784"
+
+
+def test_invert1() -> None:
+    """Invert Test 1."""
+    dict1 = {'UNC': 'Duke', 'Clemson': 'NC State'}
+    assert invert(dict1) == {'Duke': 'UNC', 'NC State': 'Clemson'}
+
+
+def test_invert2() -> None:
+    """Invert Test 2."""
+    dict2 = {'blue': 'yellow'}
+    assert invert(dict2) == {'yellow': 'blue'}
+    
+
+def test_invert3() -> None:
+    """Invert Test 3."""
+    dict3 = {'x': 'y'}
+    assert invert(dict3) == {'y': 'x'}
+
+
+def test_favorite_color1() -> None:
+    """Favorite Color Test 1."""
+    dict4 = {'Amanda': 'blue', 'Samantha': 'blue', 'Jenna': 'yellow'}
+    assert favorite_color(dict4) == 'blue'
+
+
+def test_favorite_color2() -> None:
+    """Favorite Color Test 2."""
+    dict5 = {'Anne': 'red', 'Anna': 'purple', 'Emily': 'red'}
+    assert favorite_color(dict5) == 'red'
+
+
+def test_favorite_color3() -> None:
+    """Favorite Color Test 3."""
+    dict6 = {'Caroline': 'green', 'Riley': 'black', 'Isabella': 'black', 'Forbes': 'green'}
+    assert favorite_color(dict6) == 'green'
+
+
+def test_count1() -> None:
+    """Count Test 1."""
+    list1 = ['Virginia', 'Sarah', 'Margaret', 'Katie', 'Margaret', 'Sarah']
+    assert count(list1) == {'Virginia': 1, 'Sarah': 2, 'Margaret': 2, 'Katie': 1}
+
+
+def test_count2() -> None:
+    """Count Test 2."""
+    list2 = ['Anne', 'Addie']
+    assert count(list2) == {'Anne': 1, 'Addie': 1}
+
+
+def test_count3() -> None:
+    """Count Test 3."""
+    list3 = ['bed', 'chair', 'chair', 'table', 'chair']
+    assert count(list3) == {'bed': 1, 'chair': 3, 'table': 1}
